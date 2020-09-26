6b0ddbe3e7aba3014d0305f05fbc30ab8aef4015
Juan Llorens <juan.llorens@williamhill.com>
2020-09-26 16:24:07 +0200

    Added basic config

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <div class="app" :class="period">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {},
+
+  data() {
+    return {
+      date: new Date
+    }
+  },
+
+  computed: {
+    year() {
+      return this.date.getFullYear();
+    },
+
+    period() {
+      let hour = this.date.getHours();
+
+      return (hour > 5 && hour < 18)? 'app--day': 'app--night';
+    }
+  }
+}
+</script>
+
+<style>
+/** Global **/
+@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Open Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: rgba(255, 255, 255, 0.9);
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+  transition: color .2s ease-in;
+}
+
+/** App **/
+.app {
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.app--day {
+  /*background: linear-gradient(to bottom right, #6CB9C8, #6CB9C8) no-repeat;*/
+  background-color: #6CB9C8;
+}
+
+.app--night {
+  /*background: linear-gradient(to bottom right, #484F60, #484F60) no-repeat;*/
+  background-color: #484F60;
+}
+
+.app--day a:hover {
+  color: rgba(46, 146, 167, 0.9);
+}
+
+.app--night a:hover {
+  color: rgba(0, 0, 0, 0.5);
+}
+</style>
